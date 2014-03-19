a635e19f19526f3d3d741adec1a6888f7a5a820a
Cavin <cstone2003@hotmail.com>
2014-03-19 00:46:21 -0500

    Modify DynamicContent in ActiveAdmin

--- app/admin/dynamic_contents.rb
+++ app/admin/dynamic_contents.rb
@@ -1,3 +1,39 @@
 ActiveAdmin.register DynamicContent do
-  
+  index do
+    selectable_column
+    column :title
+    column :key
+    column :value
+    default_actions
+  end
+
+  form do |f|
+    f.inputs do
+      f.input :title
+      f.input :key
+      f.input :value, as: :html_editor
+    end
+    f.actions
+  end
+
+  filter :title
+  filter :key
+  filter :value
+  filter :created_at
+  filter :updated_at
+
+  show do |dynamiccontent|
+    attributes_table do
+      row :id
+      row :title
+      row :key
+      row :value do
+        simple_format dynamiccontent.value
+      end
+      row :created_at
+      row :updated_at
+    end
+    active_admin_comments
+  end
+
 end

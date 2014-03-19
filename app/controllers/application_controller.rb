4b9efb605e235ed6baf020a9f1e871ef28a288c0
Cavin <cstone2003@hotmail.com>
2014-03-19 08:28:25 -0500

    Add get_content before_filter to application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,15 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  before_filter :get_dynamic_content
+
+  def get_dynamic_content
+    @site_title = DynamicContent.get_value(:site_title)
+    @home_title = DynamicContent.get_value(:home_title)
+    @home_content = DynamicContent.get_value(:home_content)
+    @banner_name = DynamicContent.get_value(:banner_name)
+    @banner_content = DynamicContent.get_value(:banner_content)
+    @contact_name = DynamicContent.get_value(:contact_name)
+    @contact_content = DynamicContent.get_value(:contact_content)
+  end
 end

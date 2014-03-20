068c6c386fc97f5d71a07ae913454edfa135c51e
Cavin <cstone2003@hotmail.com>
2014-03-20 17:53:56 -0500

    Remove token from gitignore

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Stizelwebs::Application.config.secret_token = '71d510520524695c4dab547a97bfa23c42a1a93e97a6f609df4d2b90800048079b2cc201ccaa7fbb9a575aa5d5dbaf92e4769aee2e30557d953d8ede7d267bbc'

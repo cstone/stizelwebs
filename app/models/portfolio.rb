be63c447d01dda38f6671981a435b23b1627edfd
Cavin <cstone2003@hotmail.com>
2014-03-19 19:31:03 -0500

    Add portolio image uploader

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -1,3 +1,5 @@
 class Portfolio < ActiveRecord::Base
   attr_accessible :description, :portfolio_image, :string, :title, :website, :remove_portfolio_image
+
+  mount_uploader :portfolio_image, PortfolioImageUploader
 end

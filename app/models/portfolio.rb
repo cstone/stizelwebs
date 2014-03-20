f029488b34b0a0e2598e11593661cc3b14e14ff4
Cavin <cstone2003@hotmail.com>
2014-03-20 12:32:12 -0500

    Add to_param to change param to title for portfolio model

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -2,4 +2,8 @@ class Portfolio < ActiveRecord::Base
   attr_accessible :description, :portfolio_image, :string, :title, :website, :remove_portfolio_image
 
   mount_uploader :portfolio_image, PortfolioImageUploader
+
+  def to_param
+    title
+  end
 end

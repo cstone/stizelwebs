443753a02a4688d47755887250b57bc887440fa5
Cavin <cstone2003@hotmail.com>
2014-03-20 17:28:00 -0500

    Add slug to_param to portfolio model

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -1,9 +1,11 @@
 class Portfolio < ActiveRecord::Base
-  attr_accessible :description, :portfolio_image, :string, :title, :website, :remove_portfolio_image
+  attr_accessible :description, :portfolio_image, :slug, :string, :title, :website, :remove_portfolio_image
 
   mount_uploader :portfolio_image, PortfolioImageUploader
 
+
+
   def to_param
-    title
+    slug
   end
 end

4f6c1d5489dcf391547dde4e347528cb77a65389
Cavin <cstone2003@hotmail.com>
2014-03-20 17:28:20 -0500

    Add find_by_slug! to portfolios controller

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -11,7 +11,7 @@ class PortfoliosController < ApplicationController
   end
 
   def show
-    @portfolio = Portfolio.find_by_title!(params[:id])
+    @portfolio = Portfolio.find_by_slug!(params[:id])
 
     respond_to do |format|
       format.html # show.html.erb

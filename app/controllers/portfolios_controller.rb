3b956970be25584b4839088db3e9c9d7847a1044
Cavin <cstone2003@hotmail.com>
2014-03-20 11:52:18 -0500

    Add json to portfolios

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -3,9 +3,19 @@ class PortfoliosController < ApplicationController
 
   def index
     @portfolios = Portfolio.all
+
+    respond_to do |format|
+      format.html # index.html.erb
+      format.json { render json: @portfolios }
+    end
   end
 
   def show
     @portfolio = Portfolio.find(params[:id])
+
+    respond_to do |format|
+      format.html # show.html.erb
+      format.json { render json: @portfolio }
+    end
   end
 end

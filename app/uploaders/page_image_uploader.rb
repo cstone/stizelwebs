8afdb5bd1a788f30c31dd584e001abf1d9d49d0b
Cavin <cstone2003@hotmail.com>
2014-04-25 17:08:36 -0500

    Resize page image to 350 pixels

--- app/uploaders/page_image_uploader.rb
+++ app/uploaders/page_image_uploader.rb
@@ -36,7 +36,7 @@ class PageImageUploader < CarrierWave::Uploader::Base
   #   "/images/fallback/" + [version_name, "default.png"].compact.join('_')
   # end
 
-  process :resize_to_limit => [250, 0]
+  process :resize_to_limit => [350, 0]
   # Process files as they are uploaded:
   # process :scale => [200, 300]
   #

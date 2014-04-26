83ebf6f8997673950e19532674c0a26d991a212a
Cavin <cstone2003@hotmail.com>
2014-04-26 16:47:36 -0500

    Change contact us initializer file with my variables

--- config/initializers/contact_us.rb
+++ config/initializers/contact_us.rb
@@ -6,25 +6,25 @@ ContactUs.setup do |config|
   # Configure the e-mail address which email notifications should be sent from.  If emails must be sent from a verified email address you may set it here.
   # Example:
   # config.mailer_from = "contact@please-change-me.com"
-  config.mailer_from = nil
+  config.mailer_from = "info@stizelwebs.com"
 
   # Configure the e-mail address which should receive the contact form email notifications.
-  config.mailer_to = "contact@please-change-me.com"
+  config.mailer_to = "info@stizelwebs.com"
 
   # ==> Form Configuration
 
   # Configure the form to ask for the users name.
-  config.require_name = false
+  config.require_name = true
 
   # Configure the form to ask for a subject.
-  config.require_subject = false
+  config.require_subject = true
 
   # Configure the form gem to use.
   # Example:
   # config.form_gem = 'formtastic'
-  config.form_gem = nil
+  config.form_gem = 'formtastic'
 
   # Configure the redirect URL after a successful submission
-  config.success_redirect = '/'
+  config.success_redirect = '/thank-you'
 
 end
